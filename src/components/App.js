da56589c0d139d0d032afa9c06e9c4f594526bdc
micchy466 <48757027+micchy466@users.noreply.github.com>
2022-12-12 23:58:57 +0900

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,18 +13,47 @@ function App() {
     };
     fetchData()
   }, []);
+
   console.log("TODOリスト:", todoList);
+
+  const inCompletedList = todoList.filter((todo) => {
+    return !todo.done;
+  });
+
+  console.log("未完了TODOリスト:", inCompletedList);
+
+  const completedList = todoList.filter((todo) => {
+    return todo.done;
+  });
+
+  console.log("完了TODOリスト:", completedList);
 
   return (
     <>
       <h1>TODO進捗管理</h1>
       <textarea />
       <button>+ todoを追加</button>
-      <h2>TODOリスト</h2>
+      <h2>未完了TODOリスト</h2>
       <ul>
-        {todoList.map((todo) => (
+        {inCompletedList.map((todo) => (
           <li key={todo.id}>
-            {todo.content} ({todo.done && "完了" || "未完了"})
+            {todo.content}
+            <button>
+              {todo.done ? "未完了リストへ" : "完了リストへ"}
+            </button>
+            <button>削除</button>
+          </li>
+        ))}
+      </ul>
+      <h2>完了TODOリスト</h2>
+      <ul>
+        {completedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}
+            <button>
+              {todo.done ? "未完了リストへ" : "完了リストへ"}
+            </button>
+            <button>削除</button>
           </li>
         ))}
       </ul>
