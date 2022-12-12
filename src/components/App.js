d838902aab9d04813b012f2cec83c8149f3cabb5
micchy466 <48757027+micchy466@users.noreply.github.com>
2022-12-12 19:56:52 +0900

    ✨ 一覧表示

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const todoDataUrl = "http://localhost:3100/todos";
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(todoDataUrl);
+      setTodoList(response.data);
+    };
+    fetchData()
+  }, []);
+  console.log("TODOリスト:", todoList);
+
+  return (
+    <>
+      <h1>TODO進捗管理</h1>
+      <textarea />
+      <button>+ todoを追加</button>
+      <h2>TODOリスト</h2>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content} ({todo.done && "完了" || "未完了"})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+
+};
+
+export default App;
